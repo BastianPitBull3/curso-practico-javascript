7b90eb877d0bfd7bf56a478b2bcd49c8c6950c5b
Diego Valle <dvallebenavides@gmail.com>
2022-07-02 17:32:55 -0500

    Se agregó un afunción para calcular la altura de triángulos isósceles

--- README.md
+++ README.md
@@ -8,3 +8,10 @@
 - Segundo paso: implementar las fórmulas en JavaScript
 - Tercer paso: Crear funciones
 - Cuarto paso: Integrar JS con HTML
+
+## Taller #2: Precios y descuentos
+
+- Primer paso: definir las formas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar JS con HTML
--- figuras.js
+++ figuras.js
@@ -38,6 +38,21 @@ function areaTriangulo(base, altura){
     return (base * altura) / 2;
 }
 //console.log("El área del triángulo mide: " + areaTriangulo + "cm^2");
+
+function isosceles(base, lado1, lado2){
+    var a , b;
+    var altura;
+    var resultado;
+
+    if (base < lado1 && lado1 === lado2){
+        a = lado1, b = base;
+        altura = Math.sqrt(Math.pow(a, 2) - (Math.pow(b, 2) / 4));
+    }else {
+        altura = null;
+    }
+
+    return altura;    
+}
 console.groupEnd();
 
 //Código del círculo
@@ -118,6 +133,25 @@ function calcularAreaTriangulo(){
 
     const area = areaTriangulo(base, altura);
     alert(area);
+}
+
+function calcularAlturaTriangulo(){
+    const input1 = document.getElementById("inputBaseTriangulo");
+    const input2 = document.getElementById("inputLadoTriangulo1");
+    const input3 = document.getElementById("inputLadoTriangulo2");
+
+    const base = input1.value;
+    const lado1 = input2.value;
+    const lado2 = input3.value;
+
+    const altura = isosceles(base, lado1, lado2);
+    if (altura){
+        document.getElementById("inputAlturaTriangulo").value = altura;
+        alert("Es un triángulo isósceles \n Altura: " + altura);
+    }else{
+        document.getElementById("inputAlturaTriangulo").value = null;
+        alert("No es un triángulo isósceles");
+    }
 }
 
  // Funciones para el círculo
