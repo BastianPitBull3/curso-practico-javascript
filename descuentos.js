d52bb8ab2cb95ce97932260c8c5064f9864648aa
Diego Valle <dvallebenavides@gmail.com>
2022-07-07 17:38:58 -0500

    Se agregó la función de ingresar cupones con sus respectivos descuentos y se volvió más legible el código

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,22 @@
-//const precioOriginal = 100;
-//const descuento = 15;
+const coupons = [
+    {
+        name: "DESC1",
+        discount: 15
+    },
+    {
+        name: "DESC2",
+        discount: 20
+    },{
+        name: "DESC3",
+        discount: 30
+    },{
+        name: "DESC4",
+        discount: 50
+    },{
+        name: "DESC5",
+        discount: 60
+    }
+    ];
 
 function calcularPrecioConDescuento(precio, descuento) {
     porcentajePrecioConDescuento = 100 - descuento;
@@ -9,14 +26,34 @@ function calcularPrecioConDescuento(precio, descuento) {
 }
 
 function calculatePrice(){
+    
     const inputPrice = document.getElementById("inputPrice");
     const priceValue = inputPrice.value;
 
-    const inputDiscount =  document.getElementById("inputDiscount")
-    const discountValue = inputDiscount.value;
+    const inputDiscount =  document.getElementById("inputDiscount");
+    var discountValue = inputDiscount.value;
 
-    const discountPrice = calcularPrecioConDescuento(priceValue, discountValue);
+    const inputCupon = document.getElementById("inputCupón");
+    const couponValue = inputCupon.value;
 
     const PResult = document.getElementById("p-result");
+    const PDescuentoCupon = document.getElementById("p-descuento-cupón");
+
+    const isCouponValueValid = function (coupon) {
+        return coupon.name === couponValue;
+    };
+    
+    const userCoupon = coupons.find(isCouponValueValid);
+
+    if (!userCoupon && discountValue == null) {
+        alert("El cupón " + couponValue + " no es válido");
+    }
+    else {
+        discountValue = userCoupon.discount;
+        PDescuentoCupon.innerText = "El descuento con cupón es del " 
+                                                                + discountValue + "%";
+    }
+
+    const discountPrice = calcularPrecioConDescuento(priceValue, discountValue);
     PResult.innerText = "El precio con descuento es de : $" + discountPrice;
 }
