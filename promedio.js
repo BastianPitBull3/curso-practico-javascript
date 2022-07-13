ccf73af360594c513a96855de5c177edecb4779c
Diego Valle <dvallebenavides@gmail.com>
2022-07-13 17:53:28 -0500

    Se agregaron los archivos mediana.html y mediana.js

--- promedio.js
+++ promedio.js
@@ -1,9 +1,16 @@
 function calcularMediaAritmética (lista) {
-    let sumaLista = 0;
+
+    /*let sumaLista = 0;
 
     for (let i = 0; i < lista.length; i++) {
         sumaLista += lista[i];
-    }
+    }*/
+
+    const sumaLista = lista.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+            return valorAcumulado + nuevoElemento;
+        }
+    )
     
     const promedioLista = sumaLista / lista.length;
 
